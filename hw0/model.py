5da033ab6919a4fb6698438dd4495c5268af59a2
BillySilver <BillySilver@users.noreply.github.com>
2017-11-01 14:39:32 +0800

    [0] Trial 1: [3x, 32]*2 - [3x, 64]*2 | 256-10.

--- hw0/model.py
+++ hw0/model.py
@@ -21,14 +21,27 @@ except:
     # Building a Convolutional Neural Network.
     model = Sequential()
     model.add(Conv2D(filters=32,
-                     kernel_size=(4, 4),
+                     kernel_size=(3, 3),
                      data_format='channels_first',
                      input_shape=(1, 28, 28),
                      activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(filters=32,
+                     kernel_size=(3, 3),
+                     activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.25))
+
+    model.add(Conv2D(filters=64,
+                     kernel_size=(3, 3),
+                     activation='relu'))
+    model.add(Conv2D(filters=64,
+                     kernel_size=(3, 3),
+                     activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.25))
 
     model.add(Flatten())
+    model.add(Dense(units=256, activation='relu'))
     model.add(Dropout(rate=0.5))
     model.add(Dense(units=10, activation='softmax'))
 
