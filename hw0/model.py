5c9de42593e29381c19440ad31ce098fb2eaf2c6
BillySilver <BillySilver@users.noreply.github.com>
2017-11-01 15:53:34 +0800

    [0] Trial 2: [4x, 128]*2, /2 - [4x, 256]*2, /2 | 10.

--- hw0/model.py
+++ hw0/model.py
@@ -20,29 +20,31 @@ try:
 except:
     # Building a Convolutional Neural Network.
     model = Sequential()
-    model.add(Conv2D(filters=32,
-                     kernel_size=(3, 3),
+    model.add(Conv2D(filters=128,
+                     kernel_size=(4, 4),
+                     padding='same',
                      data_format='channels_first',
                      input_shape=(1, 28, 28),
                      activation='relu'))
-    model.add(Conv2D(filters=32,
-                     kernel_size=(3, 3),
+    model.add(Conv2D(filters=128,
+                     kernel_size=(4, 4),
+                     padding='same',
                      activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(rate=0.25))
 
-    model.add(Conv2D(filters=64,
-                     kernel_size=(3, 3),
+    model.add(Conv2D(filters=256,
+                     kernel_size=(4, 4),
+                     padding='same',
                      activation='relu'))
-    model.add(Conv2D(filters=64,
-                     kernel_size=(3, 3),
+    model.add(Conv2D(filters=256,
+                     kernel_size=(4, 4),
+                     padding='same',
                      activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(rate=0.25))
 
     model.add(Flatten())
-    model.add(Dense(units=256, activation='relu'))
-    model.add(Dropout(rate=0.5))
     model.add(Dense(units=10, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='nadam', metrics=['accuracy'])
