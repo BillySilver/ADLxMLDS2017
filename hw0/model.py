569712c43830831afb40de28759e12e8538ee28a
BillySilver <BillySilver@users.noreply.github.com>
2017-11-02 14:27:06 +0800

    [0] Trial 9: [4x, 128]*2, /2 - [4x, 256]*2, /2 | 512-10.

--- hw0/model.py
+++ hw0/model.py
@@ -20,11 +20,26 @@ try:
 except:
     # Building a Convolutional Neural Network.
     model = Sequential()
-    model.add(Conv2D(filters=16,
+    model.add(Conv2D(filters=128,
                      kernel_size=(4, 4),
                      padding='same',
                      data_format='channels_first',
                      input_shape=(1, 28, 28),
+                     activation='relu'))
+    model.add(Conv2D(filters=128,
+                     kernel_size=(4, 4),
+                     padding='same',
+                     activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(rate=0.25))
+
+    model.add(Conv2D(filters=256,
+                     kernel_size=(4, 4),
+                     padding='same',
+                     activation='relu'))
+    model.add(Conv2D(filters=256,
+                     kernel_size=(4, 4),
+                     padding='same',
                      activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(rate=0.5))
