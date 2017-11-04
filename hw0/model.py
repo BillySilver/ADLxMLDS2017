f529dde4434663adc099d86b403d277c451eedb6
BillySilver <BillySilver@users.noreply.github.com>
2017-11-04 14:11:13 +0800

    Trial 8: [4x, 16], /2 | 512-10.

--- hw0/model.py
+++ hw0/model.py
@@ -23,23 +23,16 @@ except:
 
     # Building a Convolutional Neural Network.
     model = Sequential()
-    model.add(Conv2D(filters=128,
+    model.add(Conv2D(filters=16,
                      kernel_size=(4, 4),
                      padding='same',
                      input_shape=(1, 28, 28),
                      activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Dropout(rate=0.25))
-
-    model.add(Conv2D(filters=256,
-                     kernel_size=(4, 4),
-                     padding='same',
-                     activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(rate=0.5))
 
     model.add(Flatten())
-    model.add(Dense(units=1024, activation='relu'))
+    model.add(Dense(units=512, activation='relu'))
     model.add(Dropout(rate=0.5))
     model.add(Dense(units=10, activation='softmax'))
 
