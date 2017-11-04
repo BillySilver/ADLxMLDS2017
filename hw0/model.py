b0a4381f188c8cf7f45b8e713a0b0f0ab892628d
BillySilver <BillySilver@users.noreply.github.com>
2017-11-04 14:38:30 +0800

    [0] Trial 4: [4x, 32], /2 - [4x, 64], /2 | 512-10.

--- hw0/model.py
+++ hw0/model.py
@@ -29,10 +29,17 @@ except:
                      input_shape=(1, 28, 28),
                      activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(rate=0.25))
+
+    model.add(Conv2D(filters=64,
+                     kernel_size=(4, 4),
+                     padding='same',
+                     activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(rate=0.5))
 
     model.add(Flatten())
-    model.add(Dense(units=1024, activation='relu'))
+    model.add(Dense(units=512, activation='relu'))
     model.add(Dropout(rate=0.5))
     model.add(Dense(units=10, activation='softmax'))
 
