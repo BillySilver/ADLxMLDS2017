e4a746d438153fa15bc3328cd5b61b35a9c78d2e
BillySilver <BillySilver@users.noreply.github.com>
2017-11-04 17:28:29 +0800

    [0] Trial 15: [4x, 64]*2, /2 - [4x, 128]*2, /2 | 1024-10.

--- hw0/model.py
+++ hw0/model.py
@@ -23,23 +23,23 @@ except:
 
     # Building a Convolutional Neural Network.
     model = Sequential()
-    model.add(Conv2D(filters=32,
+    model.add(Conv2D(filters=64,
                      kernel_size=(4, 4),
                      padding='same',
                      input_shape=(1, 28, 28),
                      activation='relu'))
-    model.add(Conv2D(filters=32,
+    model.add(Conv2D(filters=64,
                      kernel_size=(4, 4),
                      padding='same',
                      activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(rate=0.25))
 
-    model.add(Conv2D(filters=64,
+    model.add(Conv2D(filters=128,
                      kernel_size=(4, 4),
                      padding='same',
                      activation='relu'))
-    model.add(Conv2D(filters=64,
+    model.add(Conv2D(filters=128,
                      kernel_size=(4, 4),
                      padding='same',
                      activation='relu'))
@@ -47,7 +47,7 @@ except:
     model.add(Dropout(rate=0.5))
 
     model.add(Flatten())
-    model.add(Dense(units=256, activation='relu'))
+    model.add(Dense(units=1024, activation='relu'))
     model.add(Dropout(rate=0.5))
     model.add(Dense(units=10, activation='softmax'))
 
