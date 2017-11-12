5d09fb89ab21fca98406853ea95d6b147198b0cc
BillySilver <BillySilver@users.noreply.github.com>
2017-11-12 22:30:23 +0800

    [2] submission for Special Mission. back to previous trial due to some error of RecurrentWrapper. 70ed013403642edcaeccf716192c04cadced9795

--- hw2/hw2_special.sh
+++ hw2/hw2_special.sh
@@ -1,3 +1,10 @@
+if [ ! -d "models" ]; then
+    mkdir models
+fi
+if [ ! -e "models/model.h5" ]; then
+    wget -O models/model.h5 'https://www.dropbox.com/s/1ofx29b7o6bji71/model03.h5?dl=1'
+fi
+
 version=$(python -V 2>&1 | grep -Po '(?<=Python )\d')
 if [ $version -eq 3 ]; then
     PYTHON=python
